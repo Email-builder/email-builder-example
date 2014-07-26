94795a609884a57522ee862627728ad28b20d016
Steven John Miller <steven.jmiller@gmail.com>
2014-07-26 16:48:09 +1000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compile Jade
+    // ----------------------
+
+    jade: {
+      email: {
+        options: {
+          pretty: true,
+          data: {
+            timestamp: "<%= new Date().getTime() %>"
+          }
+        },
+        files: [{
+          expand: true,           // Enable dynamic expansion.
+          cwd: 'src/jade',        // Src matches are relative to this path.
+          src: ['*.jade'],        // Actual pattern(s) to match.
+          dest: 'dist/html/',     // Destination path prefix.
+          ext: '.html',           // Dest filepaths will have this extension.
+        }]
+      }
+    },
+
+    // Compile Less
+    // ----------------------
+
+    less: {
+      development: {
+        options: {
+          paths: ["assets/css"]
+        },
+        files: [{
+          expand: true,           // Enable dynamic expansion.
+          cwd: 'src/less',        // Src matches are relative to this path.
+          src: ['*.less'],        // Actual pattern(s) to match.
+          dest: 'dist/css/',     // Destination path prefix.
+          ext: '.css',           // Dest filepaths will have this extension.
+        }]
+      }
+    },
+
+    // Email Builder
+    // ----------------------
+
+    emailBuilder: {
+      test :{
+        files: [{
+          expand: true,           // Enable dynamic expansion.
+          cwd: 'dist/html',        // Src matches are relative to this path.
+          src: ['*.html'],        // Actual pattern(s) to match.
+          dest: 'dist/email/',     // Destination path prefix.
+          ext: '.html',           // Dest filepaths will have this extension.
+        }]
+      }
+    },
+
+    // Watch task
+    // ----------------------
+
+    watch: {
+      jade: {
+        files: 'src/jade/**/*.jade',
+        tasks: ['jade', 'emailBuilder']
+      },
+      less: {
+        files: 'src/less/**/*.less',
+        tasks: ['less', 'emailBuilder']
+      },
+      grunt: {
+        files: ['Gruntfile.js']
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  require('load-grunt-tasks')(grunt);
+
+  // Default task(s).
+  grunt.registerTask('default', ['jade']);
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ email-builder-example
 =====================
 
 Example of how to use grunt email builder
+
+
+Make into a yeoman repo when ready
